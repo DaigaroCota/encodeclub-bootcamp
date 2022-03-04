00e5d2007ce6ffe0291652ddc106ad02f542f4ba
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-03-04 11:13:15 -0500

    config updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require('@openzeppelin/hardhat-upgrades');
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -34,4 +35,7 @@ module.exports = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false
+  }
 };
